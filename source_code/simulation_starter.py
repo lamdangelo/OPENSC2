375dbeeeb21c111c7343b7e00893fdf81b96b504
Laura D'Angelo <laura-anna-maria.dangelo@ipp.mpg.de>
2026-06-02 16:26:52 +0200

    fix: fixed relative path problem

--- source_code/simulation_starter.py
+++ source_code/simulation_starter.py
@@ -1,11 +1,13 @@
 import logging
 import logging.config
+from pathlib import Path
 
 from opensc2_gui import OPENSC2_GUI
 
 # from opensc2_gui_simpl import OPENSC2_GUI
 
-logging.config.fileConfig(fname="logging_opensc2.conf", disable_existing_loggers=True)
+config_path = Path(__file__).with_name("logging_opensc2.conf")
+logging.config.fileConfig(fname=config_path, disable_existing_loggers=True)
 
 # Get the logger specified in the file, this will be the parent logger.
 logger = logging.getLogger("opensc2Logger")
